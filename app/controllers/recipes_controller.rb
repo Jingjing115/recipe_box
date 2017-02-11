0856d339a79232bc6c1809a25ae008fc15085f90
xujing <xujing@huantengsmart.com>
2017-02-11 23:50:01 +0800

    add user devise

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @recipe = Recipe.all.order("created_at DESC")
   end
@@ -8,7 +9,8 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @recipe = Recipe.new
+    # @recipe = Recipe.new
+    @recipe = current_user.recipes.build
   end
 
   def edit
@@ -28,7 +30,8 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.new(recipe_params)
+    # @recipe = Recipe.new(recipe_params)
+    @recipe = current_user.recipes.build(recipe_params)
 
     if @recipe.save
       redirect_to @recipe, notice: "Successfully created new recipe"
